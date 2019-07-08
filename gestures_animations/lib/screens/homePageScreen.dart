6d7a1577c8f9b61f811b1f21b21453ea0a676e62
furkandeveloper <Furkan.dvlp@gmail.com>
2019-07-08 15:00:19 +0300

    animation

--- gestures_animations/lib/screens/homePageScreen.dart
+++ gestures_animations/lib/screens/homePageScreen.dart
@@ -7,14 +7,50 @@ class HomePageScreen extends StatefulWidget {
   State<StatefulWidget> createState() => HomePageScreenState();
 }
 
-class HomePageScreenState extends State {
+class HomePageScreenState extends State<HomePageScreen>
+    with SingleTickerProviderStateMixin {
   int tabsCount = 0;
   int doubleTabsCount = 0;
   int longPressCount = 0;
-  double xPosition = 40.0;
-  double yPosition = 60.0;
+  double xPosition = 0.0;
+  double yPosition = 0.0;
+  double boxSize = 0.0;
+  double fullBoxSize = 150.0;
+
+  AnimationController animationController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 10000),
+        // gereksiz kaynakları kullanımını kapatır.
+        // ancak kullanabilmek için SingleTickerProviderStateMixin with etmemiz gerekir
+        // with edilen bir class inherit edilmiş gibi davranacaktır
+        vsync: this);
+    animation =
+        CurvedAnimation(parent: animationController, curve: Curves.bounceInOut);
+        animation.addListener((){
+          setState(() {
+           boxSize=fullBoxSize*animation.value; 
+          });
+          centerBox(context);
+        });
+        // çalış
+        animationController.forward();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // if (xPosition==0.0) {
+    //   centerBox(context);
+    // }
     return Scaffold(
       // bottom overflowed hatası için çözüm;
       resizeToAvoidBottomPadding: false,
@@ -61,8 +97,8 @@ class HomePageScreenState extends State {
               left: xPosition,
               top: yPosition,
               child: Container(
-                width: 100.0,
-                height: 100.0,
+                width: boxSize,
+                height: boxSize,
                 decoration: BoxDecoration(color: Colors.brown),
               ),
             )
@@ -77,5 +113,17 @@ class HomePageScreenState extends State {
         ),
       ),
     );
+  }
+
+  void centerBox(BuildContext context) {
+    // ekranın tam ortası
+    //Media query o context ile alakalı sorgulama yapabilir
+    xPosition = (MediaQuery.of(context).size.width / 2.0) - (boxSize / 2.0);
+    yPosition =
+        (MediaQuery.of(context).size.height / 2.0) - (boxSize / 2.0 - 30.0);
+    setState(() {
+      this.xPosition = xPosition;
+      this.yPosition = yPosition;
+    });
   }
 }
